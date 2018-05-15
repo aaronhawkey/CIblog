b0564ccf7f0aaf9cf3c2bcf7843ca83841098df9
aaronhawkey48 <aaronhawkey48@gmail.com>
2018-05-14 22:50:02 -0500

    Create post addition - Redirect bug still present

---
+++ application/views/posts/create.php
@@ -0,0 +1,15 @@
+<h1><?= $title; ?> </h1>
+
+<?php echo validation_errors(); ?>
+
+<form action="create" method="post" accept-charset="utf-8">
+  <div class="form-group">
+    <label >Title</label>
+    <input type="text" class="form-control" name="title" placeholder="Add Title">
+  </div>
+  <div class="form-group">
+    <label >Body</label>
+    <textarea class="form-control" name="body" placeholder="Add Post Body"></textarea>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
